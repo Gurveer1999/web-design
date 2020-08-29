6610a825ae4900e0dfd7949944fc183170cd8e9f
Gurveer1999 <57304119+Gurveer1999@users.noreply.github.com>
2020-08-29 18:57:25 -0400

    assignments uploaded

---
+++ scripting-language/class_7-homework/script.js
@@ -0,0 +1,16 @@
+// Homework 1: Create a function having name `computeSubstraction` which should take 4 numbers as parameters &
+// then perform the subtration of 4 three numbers and display it to the user.
+// ****** Code starts here ******
+function computeSubstraction(x, y, z, k) {
+    return x - y - z - k;
+}
+
+console.log("Substraction:> ", computeSubstraction(100, 10, 30, 2));
+// ****** Code ends here ******
+
+// Homework 2: Create a function having name `computeMultiplication` which should take 4 numbers as parameters &
+// then perform the multiplication of 4 three numbers and display it to the user.
+// ****** Code starts here ******
+function computeMultiplication(x, y, z, k) {
+    return x * y * z * k;
+}
